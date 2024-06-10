b6fb7e035beb603ee0371797aa5b1551cee2e17e
Javier Aranda <javierarandalopez96@gmail.com>
2024-06-10 13:01:06 +0200

    solucionado automplete y creado el html de new hero

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
 import { FormControl } from '@angular/forms';
 import { Hero } from '../../interfaces/hero.interface';
+import { MatAutocompleteActivatedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'heroes-search-page',
@@ -12,6 +13,7 @@ export class SearchPageComponent{
 
   public searchInput = new FormControl('');
   public heroes:Hero[] =[];
+  public selectedHero?:Hero
 
   searchHero(){
     const value: string =this.searchInput.value || '';
@@ -23,7 +25,14 @@ export class SearchPageComponent{
 
   constructor(private heroesService:HeroesService){}
 
-
-
+  onSelectedOption(event:MatAutocompleteActivatedEvent):void{
+    if (!event.option?.value){
+      this.selectedHero = undefined;
+      return
+    }
+    const hero: Hero =event.option.value;
+    this.searchInput.setValue(hero.superhero);
+    this.selectedHero = hero;
+  }
 
 }
